e6517abfab5c054d714627a41db4be4554963ee2
Maria-del-Mar <mmvillarte@hotmail.com>
2024-09-16 11:56:21 +0200

    Read Stock Quote from Queue

--- src/main/java/guru/springframework/rabbit/rabbitstockquoteservice/service/QuoteRunner.java
+++ src/main/java/guru/springframework/rabbit/rabbitstockquoteservice/service/QuoteRunner.java
@@ -1,31 +1,57 @@
 package guru.springframework.rabbit.rabbitstockquoteservice.service;
 
+import com.rabbitmq.client.Delivery;
+import guru.springframework.rabbit.rabbitstockquoteservice.config.RabbitConfig;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import reactor.rabbitmq.Receiver;
 
 import java.time.Duration;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class QuoteRunner implements CommandLineRunner {
-    private QuoteGeneratorService quoteGeneratorService;
-    private QuoteMessageSender quoteMessageSender;
+    private final QuoteGeneratorService quoteGeneratorService;
+    private final QuoteMessageSender quoteMessageSender;
+    private final Receiver receiver;
+
 
     @Override
     public void run(String... args) throws Exception {
+        CountDownLatch latch = new CountDownLatch(25);
+
         quoteGeneratorService.fetchQuoteStream(Duration.ofMillis(100))
                 .take(25)
                 .log("Got quote")
                 .flatMap(quoteMessageSender::sendQuoteMessage)
                 .subscribe(result -> {
                     log.debug("Sent message to rabbit");
+
+                    latch.countDown();;
                 }, throwable -> {
                     log.error("Got error", throwable);
                 }, () -> {
                     log.debug("All Done");
+                });
+
+        latch.await(1, TimeUnit.SECONDS);
+
+        AtomicInteger receivedCount = new AtomicInteger();
+
+        receiver.consumeAutoAck(RabbitConfig.QUEUE)
+                .log("Msg Receiver")
+                .subscribe(msg -> {
+                    log.debug("Received Message # {} - {}", receivedCount.incrementAndGet(), new String(msg.getBody()));
+                }, throwable -> {
+                    log.debug("Error Receiving", throwable);
+                }, () -> {
+                    log.debug("Complete");
                 });
     }
 }
